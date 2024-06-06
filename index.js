290d0d75aac22ea61c08f70f9eec76a778b764db
Khayrul Islam <khayrultw@gmail.com>
2024-06-06 14:50:27 +0800

    Forward College API for Mobile Exam

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require('cors');
+const qoutesRouter = require("./routes/quotes")
+
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello from Space! 🚀");
 });
+
+app.use("/quotes", qoutesRouter)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
---
+++ routes/quotes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const quotes = require("../res/quotes.json")
+
+router.get("/",(req, res) => {
+    res.status(200).json(quotes);
+});
+
+router.get("/random", (req, res) => {
+    const rand = Math.floor(Math.random()*10)
+    res.json(quotes[rand])
+})
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+    const quote = quotes.find(q => q.id == id)
+    if(quote) {
+        res.status(200).json(quote)
+    } else {
+        res.status(404).json(null)
+    }
+})
+
+module.exports = router;
