7e1b9e52b187dfb679358820ac60f4475082af46
Khayrul Islam <khayrultw@gmail.com>
2024-06-07 19:13:09 +0800

    Updated for movies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const qoutesRouter = require("./routes/quotes")
+const moviesRouter = require("./routes/movies")
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,10 +9,11 @@ const port = process.env.PORT || 8080;
 app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("Hello from Space! 🚀");
+  res.sendFile(__dirname + "/res/quotes_doc.html")
 });
 
 app.use("/quotes", qoutesRouter)
+app.use("/movies", moviesRouter)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
---
+++ routes/movies.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+const movies = require("../res/movies.json")
+
+router.get("/",(req, res) => {
+    res.status(200).json(movies);
+});
+
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+    const movie = movies.find(q => q.id == id)
+    if(movie) {
+        res.status(200).json(movie)
+    } else {
+        res.status(404).json(null)
+    }
+})
+
+module.exports = router;
